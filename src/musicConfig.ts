74d535fc775faccda00f8f536cd3a241b6db1c57
chenyi.266 <chenyi.266@jd.com>
2025-12-24 23:55:57 +0800

    1

--- src/musicConfig.ts
+++ src/musicConfig.ts
@@ -1,4 +1,6 @@
 export const musicConfig: Record<string, string> = {
   "2025-12-16": "/audio/bulaiyebuqu.mp4",
+  "2025-12-24": "/audio/sdj_all_%5Bcut_75sec%5D.mp3",
+  "2025-12-25": "/audio/sdj_all_%5Bcut_75sec%5D.mp3",
   // 添加更多...
 };
